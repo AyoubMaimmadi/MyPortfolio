dd1aaa1115ed403c6dd985794bf2b803280c317e
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-03 15:19:20 +0000

    new

--- client/src/components/SocialMedia.js
+++ client/src/components/SocialMedia.js
@@ -1,17 +1,28 @@
 import React from 'react'
-import { BsTwitter, BsInstagram } from 'react-icons/bs'
+import { BsTwitter, BsInstagram, BsGithub } from 'react-icons/bs'
 import { FaFacebookF } from 'react-icons/fa'
 
 const SocialMedia = () => (
   <div className="app__social">
     <div>
-      <BsTwitter />
+      <a href="https://www.instagram.com/ayoubmaimmadi/" target="_blank">
+        <BsInstagram />
+      </a>
     </div>
     <div>
-      <FaFacebookF />
+      <a href="https://github.com/AyoubMaimmadi" target="_blank">
+        <BsGithub />
+      </a>
     </div>
     <div>
-      <BsInstagram />
+      <a href="https://www.facebook.com/AYOUB.MAIMMADI1/" target="_blank">
+        <FaFacebookF />
+      </a>
+    </div>
+    <div>
+      <a href="https://twitter.com/Ayoubmaimmadi2" target="_blank">
+        <BsTwitter />
+      </a>
     </div>
   </div>
 )
