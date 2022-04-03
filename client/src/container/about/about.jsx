6dcde9af0ca4829f8f2d0acb445908721ccc9912
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-03 17:33:55 +0000

    from heree

--- client/src/container/about/about.jsx
+++ client/src/container/about/about.jsx
@@ -19,8 +19,8 @@ const About = () => {
   return (
     <>
       <h2 className="head-text">
-        I Know that <span>Good Design</span> <br />
-        means <span>Good Business</span>
+        I'm up to <span>something</span> <br />
+        that will blow your <span>mind</span>
       </h2>
 
       <div className="app__profiles">
