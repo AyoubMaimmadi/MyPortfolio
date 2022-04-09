c6912cd7159c5f4d9f9aa3f057450ff6af52e44c
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-04-09 12:34:40 +0000

    new

--- client/src/components/SocialMedia.js
+++ client/src/components/SocialMedia.js
@@ -4,22 +4,34 @@ import { FaFacebookF } from 'react-icons/fa'
 
 const SocialMedia = () => (
   <div className="app__social">
-    <a href="https://www.instagram.com/ayoubmaimmadi/" target="_blank">
+    <a
+      href="https://www.instagram.com/ayoubmaimmadi/"
+      target="_blank"
+      rel="noreferrer"
+    >
       <div>
         <BsInstagram />
       </div>
     </a>
-    <a href="https://github.com/AyoubMaimmadi" target="_blank">
+    <a href="https://github.com/AyoubMaimmadi" target="_blank" rel="noreferrer">
       <div>
         <BsGithub />
       </div>
     </a>
-    <a href="https://www.facebook.com/AYOUB.MAIMMADI1/" target="_blank">
+    <a
+      href="https://www.facebook.com/AYOUB.MAIMMADI1/"
+      target="_blank"
+      rel="noreferrer"
+    >
       <div>
         <FaFacebookF />
       </div>
     </a>
-    <a href="https://twitter.com/Ayoubmaimmadi2" target="_blank">
+    <a
+      href="https://twitter.com/Ayoubmaimmadi2"
+      target="_blank"
+      rel="noreferrer"
+    >
       <div>
         <BsTwitter />
       </div>
