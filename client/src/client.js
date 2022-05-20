6b441ae90cf02aac35f67e2343c24dfacef3f8a6
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-20 12:10:04 +0100

    new

--- client/src/client.js
+++ client/src/client.js
@@ -2,12 +2,10 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-  // projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   projectId: 'zm1dihbj',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  // token: process.env.REACT_APP_SANITY_TOKEN,
   token:
     'skMENYguscLaCxW6CUmIVS71AwtoxvbzvSk1V7z6gEclMqp2bT2083fXeN8HduT9Et6KCvP4X8t9EEyRzIAHyLCylvAccsxUzWcdRCHTBoNcbPbhQJjVo1VjlZjBs7SEL3AcNLhmEZnqqtjqUZFsMtiX0qfvVugXwjmYCNOjzZVCTHa1L2Mf',
 })
