1388d05b91c96d8ca125e1f8971453a0b42e672b
AyoubMaimmadi <ayoubmaimmadi@gmail.com>
2022-05-30 01:21:28 +0100

    new

--- client/src/client.js
+++ client/src/client.js
@@ -2,11 +2,12 @@ import sanityClient from '@sanity/client'
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = sanityClient({
-  projectId: process.env.SANITY_PROJECT_ID,
+  projectId: 'zm1dihbj',
   dataset: 'production',
   apiVersion: '2022-02-01',
   useCdn: true,
-  token: process.env.SANITY_TOKEN,
+  token:
+    'skMENYguscLaCxW6CUmIVS71AwtoxvbzvSk1V7z6gEclMqp2bT2083fXeN8HduT9Et6KCvP4X8t9EEyRzIAHyLCylvAccsxUzWcdRCHTBoNcbPbhQJjVo1VjlZjBs7SEL3AcNLhmEZnqqtjqUZFsMtiX0qfvVugXwjmYCNOjzZVCTHa1L2Mf',
 })
 
 const builder = imageUrlBuilder(client)
